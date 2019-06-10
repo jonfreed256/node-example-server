9ea21a8ea51de18d2232b016efb31a50a8f30498
github username <github email address>
2019-06-10 18:29:37 -0400

    Added even more new compliments

--- server.js
+++ server.js
@@ -10,7 +10,22 @@ const complements = [
   "You are awesome!",
   "I'm really proud of you",
   "I like your style",
-  "You should be proud of yourself"
+  "You should be proud of yourself",
+  "You're more helpful than you realize.",
+  "You have a great sense of humor.",
+  "You've got an awesome sense of humor!",
+  "You are really courageous.",
+  "Your kindness is a balm to all who encounter it.",
+  "You're all that and a super-size bag of chips.",
+  "On a scale from 1 to 10, you're an 11.",
+  "You are strong.",
+  "You're even more beautiful on the inside than you are on the outside.",
+  "You have the courage of your convictions.",
+  "I'm inspired by you.",
+  "You're like a ray of sunshine on a really dreary day.",
+  "You are making a difference.",
+  "Thank you for being there for me.",
+  "You bring out the best in other people."
 ];
 
 function getRandomComplement() {
